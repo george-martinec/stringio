edc62cfe7d11c40b731b521b4a69a193bfcf5cbd
Jirka Martinec <jirka@martinec.dev>
2023-05-21 11:50:37 +0200

    init

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Stringio</div>
+</template>
+
+<script>
+export default {
+    name: "Title"
+}
+</script>
+
+<style scoped>
+    div {
+        font-weight: 800;
+        font-size: 1.5rem;
+    }
+</style>
